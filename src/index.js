8436ba043c331d25b2342594686b60522a3f7feb
eksite <denisandipod@gmail.com>
2021-04-08 18:40:32 +0300

    IT IS TRELLO DASHBOARD

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App.jsx";
+import App from "./components/App.jsx";
+import { ListsProvider } from "./context/index.jsx";
 
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ListsProvider>
+    <App />
+  </ListsProvider>,
+  document.getElementById("root")
+);
